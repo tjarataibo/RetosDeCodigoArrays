e289cf47d70532bebd20ab52179b68dc8a46fb0b
Tamara <tjarataibo@gmail.com>
2018-06-11 18:05:39 -0400

    ejercicio 3 listo

--- app.js
+++ app.js
@@ -2,7 +2,7 @@
 function computeAverageLengthOfWords(word1, word2) {
     return(word1.length+word2.length)/2;
   }
-  
+
   module.exports = computeAverageLengthOfWords;
 
 //2º ejercicio getNthElement
@@ -10,40 +10,41 @@ function getNthElement(array, n) {
     if(n === 0) return undefined;
     return array[n];
   }
-  
+
   module.exports = getNthElement;
 
 
 //3º ejercicio convertDoubleSpaceToSingle
 function convertDoubleSpaceToSingle(str) {
-   
-  }
-  
+  function convertDoubleSpaceToSingle(str) {
+  return str.split("  ").join(" ")
+}
+
   module.exports = convertDoubleSpaceToSingle;
 
 
-//4º ejercicio 
+//4º ejercicio
 
-//5º ejercicio 
+//5º ejercicio
 
-//6º ejercicio 
+//6º ejercicio
 
-//7º ejercicio 
+//7º ejercicio
 
-//8º ejercicio 
+//8º ejercicio
 
-//9º ejercicio 
+//9º ejercicio
 
-//10º ejercicio 
+//10º ejercicio
 
-//11º ejercicio 
+//11º ejercicio
 
-//12º ejercicio 
+//12º ejercicio
 
-//13º ejercicio 
+//13º ejercicio
 
-//14º ejercicio 
+//14º ejercicio
 
-//15º ejercicio 
+//15º ejercicio
 
-//16º ejercicio 
+//16º ejercicio
